fb99178a2a030efe87f6c2c5b2a8d4993f51f3c8
B-Woody <woody.skate.and.destroy@gmail.com>
2025-11-07 12:13:21 +1100

    First modifications

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux htop netcat socat radeontop;
+dnf5 swap -y nano-default-editor vim-default-editor;
 
 # Use a COPR Example:
 #
@@ -21,4 +22,5 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+systemctl enable podman.socket;
+systemctl enable tailscaled.service;
