105ad7157b51603a3bdcf04384715405c1099e6b
Brenden Wood <woody.skate.and.destroy@gmail.com>
2025-12-08 12:24:32 +1100

    Remove dynamic wallpapers to see if it fixes build storage space issue.

--- build_files/build.sh
+++ build_files/build.sh
@@ -31,25 +31,25 @@ cockpit{-system,-machines,-ostree,-podman,-selinux,-networkmanager,-storaged} \
 # Using updated fork from raul-lezameta because main project seems dead 
 # curl -s "https://raw.githubusercontent.com/raul-lezameta/Linux_Dynamic_Wallpapers/main/Easy_Install.sh" | bash
 
-echo "Downloading needed files started"
-git clone https://github.com/saint-13/Linux_Dynamic_Wallpapers.git  
-cd Linux_Dynamic_Wallpapers
-echo "Files downloaded"
+#echo "Downloading needed files started"
+#git clone https://github.com/saint-13/Linux_Dynamic_Wallpapers.git  
+#cd Linux_Dynamic_Wallpapers
+#echo "Files downloaded"
 
-if [[ -d /usr/share/backgrounds/Dynamic_Wallpapers ]]
-then 
-	rm -r /usr/share/backgrounds/Dynamic_Wallpapers
-	echo "Setting up"
-fi
+#if [[ -d /usr/share/backgrounds/Dynamic_Wallpapers ]]
+#then 
+#	rm -r /usr/share/backgrounds/Dynamic_Wallpapers
+#	echo "Setting up"
+# fi
 
-echo "Installing wallpapers..."
-cp -r ./Dynamic_Wallpapers/ /usr/share/backgrounds/
-cp ./xml/* /usr/share/gnome-background-properties/
-echo "Dynamic Wallpapers has been installed!"
-cd .. 
-echo "Deleting files used only for the installation process"
-rm -r Linux_Dynamic_Wallpapers
-echo "Wallpapers Installed"
+#echo "Installing wallpapers..."
+#cp -r ./Dynamic_Wallpapers/ /usr/share/backgrounds/
+#cp ./xml/* /usr/share/gnome-background-properties/
+#echo "Dynamic Wallpapers has been installed!"
+#cd .. 
+#echo "Deleting files used only for the installation process"
+#rm -r Linux_Dynamic_Wallpapers
+#echo "Wallpapers Installed"
 
 ## Enable VM/QEMU/libvirt ( same as ujust script )
 ## Bazzite seems to ship with libvirt and qemu-kvm installed.
